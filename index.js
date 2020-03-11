a3c8204c3d1fd07ebac76349d9f7a14d0d8c2cbe
cckk00 <cckk00@126.com>
2020-03-11 23:24:25 +0800

    测试冲突

---
+++ index.js
@@ -0,0 +1,18 @@
+function fn (){
+a=1;}
+var animal = ['白兔', '蜻蜓'];
+
+  animal.forEach(v => {
+    console.log('v ==> ', v);
+  })
+=======
+  console.log('loaded');
+  console.log('hello');
+  function fn() {
+    var a = 1;
+    return a;
+  }
+  function fn2(){
+	  var b = 2;
+	  return b;
+  }
